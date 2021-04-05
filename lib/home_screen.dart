1716c87bf17d689130f7504cb5d52519e3895e4c
齋藤諒 <ryo.saito314@gmail.com>
2021-04-05 18:10:44 +0900

    種目名(ListTile)の追加

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<String> event = ['床', 'あん馬', '吊り輪', '跳馬', '平行棒', '鉄棒'];
+  final List<String> eventEng = ['FX', 'PH', 'SR', 'VT', 'PB', 'HB'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('6種目'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          _eventTile(event[0], eventEng[0]),
+          _eventTile(event[1], eventEng[1]),
+          _eventTile(event[2], eventEng[2]),
+          _eventTile(event[3], eventEng[3]),
+          _eventTile(event[4], eventEng[4]),
+          _eventTile(event[5], eventEng[5]),
+        ],
+      ),
+    );
+  }
+
+  _eventTile(String event, String eventEng) {
+    return ListTile(
+      title: Text(
+        '$event',
+        style: TextStyle(fontSize: 20),
+      ),
+      subtitle: Text(
+        '$eventEng',
+      ),
+      onTap: () {
+        // TODO
+      },
     );
   }
 }
