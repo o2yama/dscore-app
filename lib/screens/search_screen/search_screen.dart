609a0ee655bf5420b3f656ed65260a2e707f589e
齋藤諒 <ryo.saito314@gmail.com>
2021-04-23 14:42:38 +0900

    SearchScreenのUI変更

--- lib/screens/search_screen/search_screen.dart
+++ lib/screens/search_screen/search_screen.dart
@@ -1,46 +1,145 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class SearchScreen extends StatelessWidget {
+import 'package:dscore_app/screens/calculation_screen/calculation_screen_model.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+
+import '../../ad_state.dart';
+
+class SearchScreen extends StatefulWidget {
   SearchScreen(this.event);
   final String event;
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  BannerAd? banner;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final adState = Provider.of<AdState>(context);
+    adState.initialization.then((status) {
+      setState(() {
+        banner = BannerAd(
+          adUnitId: adState.bannerAdUnitId,
+          size: AdSize.banner,
+          request: AdRequest(),
+          listener: adState.adListener,
+        )..load();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(event),
+      body: SafeArea(
+        child:
+            Consumer<CalculationScreenModel>(builder: (context, model, child) {
+          final height = MediaQuery.of(context).size.height - 50;
+          return SingleChildScrollView(
+            child: Container(
+              color: Theme.of(context).backgroundColor,
+              child: Column(
+                children: [
+                  //広告
+                  ad(context),
+                  //戻るボタン
+                  Container(
+                    height: height * 0.1,
+                    child: _backButton(context, widget.event),
+                  ),
+                  //検索バー
+                  Container(
+                    height: height * 0.2,
+                    child: _searchBar(),
+                  ),
+                  //検索結果
+                  Container(
+                    height: height * 0.2,
+                    child: _searchResults(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            //検索バー
-            _searchBar(),
-            //検索結果
-            _searchResults(),
-          ],
+    );
+  }
+
+  //広告
+  Widget ad(BuildContext context) {
+    return banner == null
+        ? Container(height: 50)
+        : Container(
+            height: 50,
+            child: AdWidget(ad: banner!),
+          );
+  }
+
+  //戻るボタン
+  _backButton(BuildContext context, String event) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Platform.isIOS
+              ? Row(
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Text(
+                      '$eventスコア計算',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                )
+              : Icon(
+                  Icons.clear,
+                  color: Theme.of(context).primaryColor,
+                ),
         ),
-      ),
+      ],
     );
   }
 
   //検索バー
   Widget _searchBar() {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 20.0),
-          child: Text(
-            '検索',
-            style: TextStyle(fontSize: 25.0),
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+          prefixIcon: Icon(Icons.search),
+          border: new OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.0),
+            ),
           ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.0),
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          hintText: '検索',
         ),
-        TextField(
-          autofocus: true,
-          decoration: InputDecoration(
-            hintText: 'コールマン',
-          ),
-        )
-      ],
+      ),
     );
   }
 
