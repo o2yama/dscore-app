2977a4b702cabe8a5a6c95f9bcf410fcf38af078
TaiseiOtsuyama <taisei.otsuyama@stv-tech.co.jp>
2022-01-13 11:45:29 +0900

    domain名の変更

--- lib/domain/current_user.dart
+++ lib/domain/current_user.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CurrentUser {
-  CurrentUser(DocumentSnapshot<Map<String, dynamic>> doc) {
+class AppUser {
+  AppUser(DocumentSnapshot<Map<String, dynamic>> doc) {
     id = doc.data()!['id'] as String;
     email = doc.data()!['email'] as String;
   }
